444f1009e6cf89ad1f00eb819c8720edafa49c84
Claudio Olivares <claudio.olivares@zentagroup.com>
2020-07-21 17:15:53 -0400

    se agregan solutions

--- src/app/components/jumbotron/jumbotron.component.ts
+++ src/app/components/jumbotron/jumbotron.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-jumbotron',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JumbotronComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+  }
+  goToContact(){
+    this.router.navigate(['contact']);
+
   }
 
 }
